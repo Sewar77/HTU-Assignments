be83bdf6b72f3eaf445d8a816dbb9d95c3c4b622
Sewar77 <sewarjihad793@gmail.com>
2025-03-23 20:44:51 +0300

    commit

--- Attendance System/AttendanceSystem.js
+++ Attendance System/AttendanceSystem.js
@@ -1,12 +1,10 @@
 let attendanceBtn = document.getElementById("attendance-btn");
-
 attendanceBtn.onclick = function () {
   let name = document.getElementById("stdName").value;
   let newName = document.createElement("li");
   newName.classList.add("border", "border-white", "p-3", "m-3");
   if (name.trim() !== "") {
     newName.innerText = "Name : " + name.charAt(0).toUpperCase() + name.slice(1) + " | Date : " + Date();
-    
     document.getElementById("attendance-names").appendChild(newName);
     document.getElementById("stdName").value = "";
   } else {
