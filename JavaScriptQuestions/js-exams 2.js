9ccc02fcec5d39ed94eb0e1fb12c48a1bfdfe610
Sewar77 <sewarjihad793@gmail.com>
2025-03-29 21:40:01 +0300

    commit js examples

--- JavaScriptQuestions/js-exams 2.js
+++ JavaScriptQuestions/js-exams 2.js
@@ -38,15 +38,44 @@ even
 odd
 even
 */
-let arrnum = [];
-function CheckOddEven() {
-  let number = document.getElementById("inputArray").value;
-  let outputarray = document.getElementById("outputArray").value;
 
-  if (number !== 0) {
-    arrnum.push(number);
-    document.getElementById("inputArray").value = "";
-    number = document.getElementById("inputArray").value;
+function oddEvenNumbers() {
+  let inputArray = document.getElementById("inputArray").value;
+  let numArr = inputArray.split(",").map(Number);
+  let result = "";
+  for (let n = 0; n < numArr.length; n++) {
+    if (numArr[n] % 2 == 0) {
+      result += " Even ";
+    } else {
+      result += " Odd ";
+    }
   }
-  outputarray;
+  document.getElementById("Result").innerHTML = result;
+}
+
+/*
+Find Longest Word in a String
+Write a function that takes a sentence 
+and returns the longest word in it.
+
+Example:
+Input: "I love JavaScript programming"
+Output: "JavaScript"
+
+*/
+
+function lognestWord() {
+  let inputSentence = document.getElementById("inputSentence").value.split(" ");
+  let maxLength = 0;
+  for (let word = 0; word < inputSentence.length;) {
+    //["Hello", "world", "this", "is", "JavaScript"]
+    maxLength = inputSentence[word].length; //hello
+    if (inputSentence[word].length > maxLength){
+      maxLength = inputSentence[word].length ;
+      word++;
+    }
+    else{
+      return inputSentence[word];
+    }
+  }
 }
