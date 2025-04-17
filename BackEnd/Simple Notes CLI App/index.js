196d1b4822984ec453289d025400ea998b4b43e2
Sewar77 <sewarjihad793@gmail.com>
2025-04-17 17:01:32 +0300

    commit the full not eCLI app

--- BackEnd/Simple Notes CLI App/index.js
+++ BackEnd/Simple Notes CLI App/index.js
@@ -15,51 +15,229 @@ Learn how to append without erasing data
 */
 
 import inquirer from "inquirer";
-import fs from "fs";
+import fs, { existsSync } from "fs";
 
-inquirer
-  .prompt([
-    {
-      message: "Enter the Title of note: ",
-      name: "title",
-    },
-  ])
-  .then((answer) => {
-    const title = answer.title;
-    fs.writeFileSync("./UserNotes.txt", "Title: " + title);
+class Note {
+  constructor() {
+    this.notes = [];
+  }
 
+  addNote() {
+    inquirer
+      .prompt([
+        {
+          message: "Add title: ",
+          name: "title",
+        },
+      ])
+      .then((answer) => {
+        const title = answer.title;
+        if (title.trim() === "") {
+          console.log("Cant accept empty title. ReInput. ");
+          return this.addNote();
+        } else {
+          if (existsSync("./Notes.json")) {
+            console.log("Saved");
+            // fs.appendFileSync("./Notes.json", "Title: " + title);
+          }
+          console.log("Note Created.");
+          inquirer
+            .prompt([
+              {
+                message:
+                  "Do you want to add description? if yes write it, if no press n. ",
+                name: "description",
+              },
+            ])
+            .then((answer) => {
+              let description = answer.description;
+              description = description.toLowerCase();
+
+              if (description.trim() === "") {
+                console.log("Note saved without description. ");
+                description = null;
+                this.noteApp();
+              }
+              const newNote = { title, description };
+
+              let data = [];
+              if (fs.existsSync("./Notes.json")) {
+                try {
+                  const file = fs.readFileSync("./Notes.json", "utf8");
+                  data = JSON.parse(file);
+                } catch (err) {
+                  console.log(
+                    "⚠️ Couldn't read existing notes. Starting fresh..."
+                  );
+                  data = [];
+                }
+              }
+
+              data.push(newNote);
+
+              fs.writeFileSync("./Notes.json", JSON.stringify(data, null, 2));
+              console.log("✅ Note saved successfully!");
+
+              this.noteApp();
+            });
+        }
+      });
+  }
+
+  viewNotes() {
+    try {
+      if (fs.existsSync("./Notes.json")) {
+        const notes = fs.readFileSync("./Notes.json", "utf8");
+        const veiw = JSON.parse(notes).map(
+          (note, index) =>
+            "\nNote " +
+            (index + 1) +
+            ":\nTitle: " +
+            note.title +
+            "  \nDescription: " +
+            note.description
+        );
+        console.log(veiw.join("\n"));
+        this.noteApp();
+      } else {
+        console.log("No notes to view yet.");
+        this.noteApp();
+      }
+    } catch (err) {
+      console.log("There is no notes yet. ");
+      this.noteApp();
+    }
+  }
+
+  deleteNote() {
+    if (fs.existsSync("./Notes.json")) {
+      const notes = fs.readFileSync("./Notes.json", "utf8");
+      const veiw = JSON.parse(notes).map(
+        (note, index) =>
+          "\nNote " +
+          (index + 1) +
+          ":\nTitle: " +
+          note.title +
+          "  \nDescription: " +
+          note.description
+      );
+      console.log(veiw.join("\n"));
+    }
+    inquirer
+      .prompt([
+        {
+          message: "Enter the number of note to delete: ",
+          name: "deletedNote",
+        },
+      ])
+      .then((answer) => {
+        const notes = fs.readFileSync("./Notes.json", "utf8");
+        const deleted = JSON.parse(notes);
+        const indexToDelete = parseInt(answer.deletedNote) - 1;
+        if (indexToDelete >= 0 && indexToDelete < deleted.length) {
+          deleted.splice(indexToDelete, 1);
+        } else {
+          console.log("Invalid note number.");
+          this.noteApp();
+          return;
+        }
+        fs.writeFileSync("./Notes.json", JSON.stringify(deleted, null, 2));
+        console.log("Note deleted successfully!");
+        this.noteApp();
+      });
+  }
+
+  editNote() {
+    if (fs.existsSync("./Notes.json")) {
+      const notes = fs.readFileSync("./Notes.json", "utf8");
+      const veiw = JSON.parse(notes).map(
+        (note, index) =>
+          "\nNote " +
+          (index + 1) +
+          ":\nTitle: " +
+          note.title +
+          "  \nDescription: " +
+          note.description
+      );
+      console.log(veiw.join("\n"));
+    }
+    inquirer
+      .prompt([
+        {
+          message: "Enter the number of note to edit: ",
+          name: "editNote",
+        },
+      ])
+      .then((answer) => {
+        const notes = fs.readFileSync("./Notes.json", "utf8");
+        const edited = JSON.parse(notes);
+        const indexToEdit = parseInt(answer.editNote) - 1;
+        if (indexToEdit >= 0 && indexToEdit < edited.length) {
+          inquirer
+            .prompt([
+              {
+                message: "Enter the new title: ",
+                name: "title",
+              },
+              {
+                message: "Enter the new description: ",
+                name: "description",
+              },
+            ])
+            .then((answer) => {
+              edited[indexToEdit].title = answer.title;
+              edited[indexToEdit].description = answer.description;
+
+              fs.writeFileSync("./Notes.json", JSON.stringify(edited, null, 2));
+              console.log("Note Updated.");
+            });
+        } else {
+          console.log("❌ Invalid note number.");
+          this.noteApp();
+        }
+        this.noteApp();
+      });
+  }
+
+  noteApp() {
     inquirer
       .prompt([
         {
           message:
-            "Want to add description? If yes, write it down. If no, press N.",
-          name: "descripe",
+            "Welcome in NotesApp, Please Choose the operation You want:\n\
+                A for Add New Note,\n \
+                V for View the Notes,\n \
+                D for Delete Note, \n\
+                E for Edit Note,   \n \
+                O for Exit App.     \n ",
+          name: "note",
         },
       ])
       .then((answer) => {
-        const descripe = answer.descripe;
-        if (descripe === "N" || descripe === "n") {
-          console.log("Done.");
-        } else {
-          const newNote = {
-            title: title,
-            description: descripe, // Fixed spelling
-          };
-          let notes = [];
-          if (fs.existsSync("./UserNotes.json")) {
-            const data = fs.readFileSync("./UserNotes.json", "utf8");
-            notes = JSON.parse(data);
-          }
-          notes.push(newNote);
-          fs.writeFileSync("./UserNotes.json", JSON.stringify(notes, null, 2));
-          fs.appendFileSync("./UserNotes.txt", "\nDescription: " + descripe);
-          console.log("Done");
+        let noteOperation = answer.note;
+        noteOperation = noteOperation.toUpperCase();
+        switch (noteOperation) {
+          case "A":
+            this.addNote();
+            break;
+          case "V":
+            this.viewNotes();
+            break;
+          case "D":
+            this.deleteNote();
+            break;
+          case "E":
+            this.editNote();
+            break;
+          case "O":
+            process.exit();
+          default:
+            console.log("Seems Wring Input, Please reinput again.");
+            this.noteApp();
         }
-      })
-      .catch((error) => {
-        console.error("An error occurred:", error);
       });
-  })
-  .catch((error) => {
-    console.error("An error occurred:", error);
-  });
+  }
+}
+
+let app = new Note();
+app.noteApp();
