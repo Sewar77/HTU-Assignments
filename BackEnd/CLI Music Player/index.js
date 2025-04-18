2245b5bd4b64ee5fba8239d9eed836e5bd870b6a
Sewar77 <sewarjihad793@gmail.com>
2025-04-18 13:09:41 +0300

    commit email sender project

--- BackEnd/CLI Music Player/index.js
+++ BackEnd/CLI Music Player/index.js
@@ -1,14 +1,39 @@
-/*
-
-- 🎶 Stream and play `.mp3` files from your folders
-- 🧠 Learn how to use `child_process` to interface with system audio players
-- 📂 Explore working deeply with the file system (reading directories, filtering audio files)
-- 📚 Use neat libraries like [`play-sound`] [`node-lame`] to decode/play audio
-
-- 💡 And you can expand it later with:
-  - ✅ Playlist management
-  - 🔀 Shuffle/Repeat options
-  - 🔎 Search music by name
-  - ⏯️ Pause/Resume commands
-
-*/
+import inquirer from "inquirer";
+import fs from "fs";
+import play from "play-sound";
+const player = play();
+class playSound {
+  constructor() {
+    this.sound = [];
+  }
+  Song() {
+    console.log("🎵 Welcome to CLI Music Player 🎵");
+    console.log("📂 Loading songs from /music...");
+    const file = fs
+      .readdirSync("./music", "utf8")
+      .filter((song) => song.endsWith(".mp3"));
+    file.forEach((file, index) => {
+      console.log(`${index + 1}. ${file}`);
+    });
+    inquirer
+      .prompt([
+        {
+          message: "Pick the number of song you want: ",
+          name: "songNumber",
+        },
+      ])
+      .then((answer) => {
+        const songNumber = parseInt(answer.songNumber);
+        const index = songNumber - 1;
+        if (index >= 0 && index < file.length) {
+          const songPath = `./music/${file[index]}`;
+          player.play(songPath);
+          console.log("Quraan is played now. ");
+        } else {
+          console.log("No Music in there. ");
+        }
+      });
+  }
+}
+let song = new playSound();
+song.Song();
